3b1469c19da269a22d35e30a96001bed4895b6ba
LuisHsu <luis8404272@gmail.com>
2015-09-24 19:32:59 +0800

    Delete temporary file and recover the makefile
    I just pushed some temporary file and the makefile changes.

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -13,14 +13,14 @@ fibonacci:
 	@ PROLOG
 	push {r3, r4, r5, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+	subs r4, r0, #0	@ R4 = R0 - 0 (update flags)
+	ble .L3			@ if(R0 <= 0) goto .L3 (which returns 0)
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+	cmp r4, #1		@ Compare R4 wtih 1
+	beq .L4			@ If R4 == 1 goto .L4 (which returns 1)
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
+	sub r0, r4, #1	@ R0 = R4 - 1
+	b fibonacci		@ Jump to fibonacci with R4 - 1 as parameter
 
 	@ R5 = R0
 	@ R0 = R4 - 2
