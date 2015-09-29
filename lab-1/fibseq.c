9eba2113e6dd71ac2674c7281b25f4994e549254
LuisHsu <luis8404272@gmail.com>
2015-09-30 03:10:56 +0800

    Test inline assembly
    Use a simple "MOV" instruction to test inline
    After this test, I can change the move instruction to count cycle.

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -4,12 +4,16 @@ extern int fibonacci(int term, int val, int prev);
 
 int main(int argc, char **argv)
 {
-  int number=0;
-  int result=0;
-
-  scanf("%d",&number);
-  result = fibonacci(number,1,0);   
-  printf("The fibonacci sequence at %d is: %d\n", number, result);
-  return 0;
+	int number=0;
+	int result=0;
+	
+	int c_start=0;
+	asm("mov %0, #2" : "=r"(c_start));
+	printf("%d\n",c_start);
+	
+	scanf("%d",&number);
+	result = fibonacci(number,1,0);
+	printf("The fibonacci sequence at %d is: %d\n", number, result);
+	return 0;
 }
 
