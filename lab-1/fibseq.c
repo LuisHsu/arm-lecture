16a4327ff1db3070727371cd40a71d8ce816e5ff
LuisHsu <luis8404272@gmail.com>
2015-09-30 10:50:14 +0800

    Count cycle clock
    Output the interval clock of fibonacci function
    I tried to use arm CCNT register to count, but qemu isn't able to use
    MRC in user-mode.
    Finally, I used clock() to get clock.

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
+#include <time.h>
 
 extern int fibonacci(int term, int val, int prev);
+
 
 int main(int argc, char **argv)
 {
 	int number=0;
-	int result=0;
-	
-	int c_start=0;
-	asm("mov %0, #2" : "=r"(c_start));
-	printf("%d\n",c_start);
+	int result=0;		
+	clock_t c_start=clock();
 	
 	scanf("%d",&number);
+	
 	result = fibonacci(number,1,0);
+	
+	printf("%d\n",clock()-c_start);
+	
 	printf("The fibonacci sequence at %d is: %d\n", number, result);
 	return 0;
 }
