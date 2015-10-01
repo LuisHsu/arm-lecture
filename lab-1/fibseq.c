d918a14e49805d5e961e71b015677a144c4b1177
LuisHsu <luis8404272@gmail.com>
2015-10-02 01:51:00 +0800

    Plot clock time
    Modify Makefile and add a script to plot clock time
    I tried to use gnuplot.
    It is convenient to see the clock count in chart.

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -7,16 +7,18 @@ extern int fibonacci(int term, int val, int prev);
 int main(int argc, char **argv)
 {
 	int number=0;
-	int result=0;		
+	unsigned int result=0;		
 	clock_t c_start=clock();
 	
 	scanf("%d",&number);
 	
 	result = fibonacci(number,1,0);
 	
-	printf("%d\n",clock()-c_start);
+	FILE* fout=fopen("clock","a");
 	
-	printf("The fibonacci sequence at %d is: %d\n", number, result);
+	fprintf(fout,"%d %d\n",number,clock()-c_start);
+	
+	printf("The fibonacci sequence at %d is: %ld\n", number, result);
 	return 0;
 }
 
